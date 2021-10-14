7855c038b705c9819148147440f5af76fec670bc
RubenEngel <66388962+RubenEngel@users.noreply.github.com>
2021-10-14 15:03:41 +0100

    update tailwind purge

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,42 +1,47 @@
-const colors = require('tailwindcss/colors');
+const colors = require("tailwindcss/colors");
 
 module.exports = {
-  purge: ['./components/**/*.js', './pages/**/*.js'],
+  purge: [
+    "./components/**/*.js",
+    "./components/**/*.tsx",
+    "./pages/**/*.js",
+    "./pages/**/*.tsx",
+  ],
   theme: {
     extend: {
       colors: {
-        'accent-1': '#FAFAFA',
-        'accent-2': '#EAEAEA',
-        'accent-7': '#333',
-        success: '#0070f3',
-        cyan: '#79FFE1',
-        primary: '#5c5c5ce6',
-        secondary: '#ffffffe8',
+        "accent-1": "#FAFAFA",
+        "accent-2": "#EAEAEA",
+        "accent-7": "#333",
+        success: "#0070f3",
+        cyan: "#79FFE1",
+        primary: "#5c5c5ce6",
+        secondary: "#ffffffe8",
       },
       spacing: {
-        28: '7rem',
+        28: "7rem",
       },
       zIndex: {
-        '-10': '-10',
+        "-10": "-10",
       },
       letterSpacing: {
-        tighter: '-.04em',
+        tighter: "-.04em",
       },
       lineHeight: {
         tight: 1.2,
       },
       minWidth: {
-        sm: '24rem',
+        sm: "24rem",
       },
       fontSize: {
-        '5xl': '2.5rem',
-        '6xl': '2.75rem',
-        '7xl': '4.5rem',
-        '8xl': '6.25rem',
+        "5xl": "2.5rem",
+        "6xl": "2.75rem",
+        "7xl": "4.5rem",
+        "8xl": "6.25rem",
       },
       boxShadow: {
-        small: '0 5px 10px rgba(0, 0, 0, 0.12)',
-        medium: '0 8px 30px rgba(0, 0, 0, 0.12)',
+        small: "0 5px 10px rgba(0, 0, 0, 0.12)",
+        medium: "0 8px 30px rgba(0, 0, 0, 0.12)",
       },
     },
   },
